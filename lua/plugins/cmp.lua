9be67fae70636f648ad2f49561e5aefaf84a4209
Your Name <you@example.com>
2025-10-20 22:55:33 +0800

    fix <TAB>

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -85,10 +85,10 @@ return {
           ["<C-e>"] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close() },
           ["<CR>"] = cmp.mapping.confirm { select = false },
           ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
+            if luasnip.expand_or_jumpable() then
               luasnip.expand_or_jump()
+            elseif cmp.visible() then
+              cmp.select_next_item()
             elseif has_words_before() then
               cmp.complete()
             else
@@ -96,10 +96,10 @@ return {
             end
           end, { "i", "s" }),
           ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
+            if luasnip.jumpable(-1) then
               luasnip.jump(-1)
+            elseif cmp.visible() then
+              cmp.select_prev_item()
             else
               fallback()
             end
